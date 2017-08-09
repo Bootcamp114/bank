5ed6db3d0f0bef256437e8a2dbbdc09d9c563700
Joevanka <Joevanka@10.8.106.41>
2017-08-09 11:04:35 +0700

    nasabah

--- src/main/java/bank/model/Nasabah.java
+++ src/main/java/bank/model/Nasabah.java
@@ -1,5 +1,20 @@
 package bank.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BANK_NASABAH")
 public class Nasabah {
 
+	private int id;
+	private int nomorKtp;
+	private String nama;
+	private String alamat;
+	private String email;
+	private String tempatLahir;
+	private Date tanggalLahir;
+	
 }
