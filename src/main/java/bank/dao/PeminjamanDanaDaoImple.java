d9b98d952a012995143584b97bbbde4e8c34c534
Joevanka <Joevanka@10.8.104.107>
2017-08-16 10:03:01 +0700

    ea

--- src/main/java/bank/dao/PeminjamanDanaDaoImple.java
+++ src/main/java/bank/dao/PeminjamanDanaDaoImple.java
@@ -29,7 +29,7 @@ public class PeminjamanDanaDaoImple implements PeminjamanDanaDao{
 		Session session = sessionFactory.getCurrentSession();
 		PeminjamanDana pinjamDn = new PeminjamanDana();
 		pinjamDn.setId(id);
-		pinjamDn.setNama(null);
+		pinjamDn.setTujuanPenggunaan(null);
 		session.delete(id);
 		session.flush();
 	}
